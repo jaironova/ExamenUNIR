3622f5123948474607748d53d74858445f818b7a
jaironova <97482683+jaironova@users.noreply.github.com>
2022-07-16 08:53:54 -0500

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,73 @@
+[
+   {
+      "alumno":"Jairo Nova",
+      "nota":9.50,
+      "ranking":10
+   },
+   {
+      "alumno":"Beatriz Basabe",
+      "nota":9.00,
+      "ranking":50
+   },
+   {
+      "alumno":"Karla Sphia Nova Basabe",
+      "nota":8.89,
+      "ranking":70,
+      "nsnc":14
+   },
+   {
+      "alumno":"Sebastian Cardenas",
+      "nota":8.00,
+      "ranking":125
+   },
+   {
+      "alumno":"Jesus Vergara",
+      "nota":8.25,
+      "ranking":250
+   },
+   {
+      "alumno":"Nicolas Sarmiento",
+      "nota":3.14,
+      "ranking":302
+   },
+   {
+      "alumno":"Gustavo Bolivar",
+      "nota":4.96,
+      "ranking":292
+   },
+   {
+      "alumno":"Cesar Rincon",
+      "nota":2.92,
+      "ranking":382
+   },
+   {
+      "alumno":"Felipe Cordoba",
+      "nota":8.33,
+      "ranking":110
+   },
+   {
+      "alumno":"Gina Bermudez",
+      "nota":4.94,
+      "ranking":260
+   },
+   {
+      "alumno":"Alejandra Roa",
+      "nota":4.96,
+      "ranking":254
+   },
+   {
+      "alumno":"Jhon Martinez",
+      "nota":2.49,
+      "ranking":390
+   },
+   {
+      "alumno":"Felipe Romero",
+      "nota":3.13,
+      "ranking":301
+   },
+   {
+      "alumno":"Marco Angarita",
+      "nota":6.57,
+      "ranking":190
+   }   
+]
